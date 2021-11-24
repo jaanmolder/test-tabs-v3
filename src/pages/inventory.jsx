def7a075178040d5f4b539bf57cadd9ef886f8a9
jaanmolder <jaan.molder@outlook.com>
2021-11-24 14:44:27 +0200

    commit

--- src/pages/inventory.jsx
+++ src/pages/inventory.jsx
@@ -1,32 +1,34 @@
 import { textInventory } from "../setup/params";
 import style from "../css/pages.module.css";
-import React from 'react';
-import {bindLifecycle} from 'react-keep-alive';
+import React from "react";
 
+const Inventory = () => {
+  return (
+    <main>
+      <div className={style.body}>
+        <h2>{textInventory}</h2>
+        <input />
+      </div>
+    </main>
+  );
+};
+
+export default Inventory;
+
+// import {bindLifecycle} from 'react-keep-alive';
 
 // @bindLifecycle
-class Inventory extends React.Component {
-    render() {
-        return (
-            <main>
-                <div className={style.body}>
-                    <h2>{textInventory}</h2>
-                    <input />
-                </div>
-            </main>
-        );
-    }
-}
-
-// const Inventory = () => {
-//   return (
-//     <main>
-//       <div className={style.body}>
-//         <h2>{textInventory}</h2>
-//         <input />
-//       </div>
-//     </main>
-//   );
-// };
+// class Inventory extends React.Component {
+//     render() {
+//         return (
+//             <main>
+//                 <div className={style.body}>
+//                     <h2>{textInventory}</h2>
+//                     <input />
+//                 </div>
+//             </main>
+//         );
+//     }
+// }
 //
-export default Inventory;
+// export default Inventory;
--- src/pages/orders.jsx
+++ src/pages/orders.jsx
@@ -1,25 +1,14 @@
-import {textInventory, textOrders} from "../setup/params";
+import { textOrders } from "../setup/params";
 import style from "../css/pages.module.css";
-import React from 'react';
+import React from "react";
 
-// const Orders = () => {
-//   return (
-//     <main className={style.body}>
-//       <h2>{textOrders}</h2>
-//       <input />
-//     </main>
-//   );
-// };
-
-class Orders extends React.Component {
-    render() {
-        return (
-            <main className={style.body}>
-                <h2>{textOrders}</h2>
-                <input />
-            </main>
-        );
-    }
-}
+const Orders = () => {
+  return (
+    <main className={style.body}>
+      <h2>{textOrders}</h2>
+      <input />
+    </main>
+  );
+};
 
 export default Orders;
